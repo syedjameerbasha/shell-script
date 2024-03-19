6dd61f6c45bffabd1647cd73df0d3d652619adc4
jameer <info@devops.com>
2024-03-19 15:47:37 +0530

    command is added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1 # this is the first value from command line
+PERSON2=$2 ## this is the second value from command line
+
+
+echo "$PERSON1: Hi, $PERSON2 good morning"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: How are you?"
+echo "$PERSON2: i am good"
