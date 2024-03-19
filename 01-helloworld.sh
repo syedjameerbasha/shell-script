466fe5228c933133bb4e5be51b9c5e592088e7a4
jameer <info@devops.com>
2024-03-19 13:16:52 +0530

    new lines removed

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -4,3 +4,4 @@ echo "hello world, i am learning devops with AWS"
 echo "hello world, i am  devops with AWS"
 echo "new lines added";
 echo "another new lines added";
+echo "another new lines removed";
