337acdd028aeab18529519712aefa458b804e089
jameer <info@devops.com>
2024-03-19 16:29:18 +0530

    command is added

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#this is the comment, we need to develop a program that can do addition of two numbers.
+
+NUMBER1=$1
+NUMBER2=$2
+# I need to run a command inside shell to add two numbers
+SUM=((NUMBER1+NUMBER2))
+
+echo "addition of two numbers is: $SUM"
